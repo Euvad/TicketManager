d04fd044273c2aa3ee4060e426e7b3d050aee64b
VAD <vad.studio@icloud.com>
2023-05-13 04:56:01 +0200

    Working on 2 new function an automatic question helper and a welcome message on ticket creation.

--- main.js
+++ main.js
@@ -12,4 +12,21 @@ loadCommands(bot);
 loadEvents(bot);
   setInterval(() => {
     loadClear(bot);
-  }, 7200000);
+  }, 720000);
+  bot.on('channelCreate', (channel) => {
+    if (channel.name.startsWith('ticket')) {
+      setTimeout(() => {
+        channel.send(`@here **Dear user**, your ticket has been successfully opened. The maximum response time is **24 hours**. Please note that your ticket will be automatically deleted after **2 days**.`);
+      }, 5000);
+    }
+  });
+
+  
+// bot.on('messageCreate', (message) => {
+//   if (message.channel.name.startsWith('ticket') && !message.member.permissions.has(Discord.PermissionFlagsBits.Administrator)) {
+//     const content = message.content.toLowerCase();
+//     if (content.includes('hwid') && content.includes('reset')) {
+//       message.channel.send('Hello, if you asked for an hwid reset please send your email and we will reset it as soon as possible.');
+//     }
+//   }
+// });
